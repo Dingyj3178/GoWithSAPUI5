e4164b3b04f4ec26e3cb32189314d13cf142ee14
Dingyj3178 <ll3178292@gmail.com>
2019-09-10 12:20:07 +0900

    version_1.0

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.12
+
+require (
+	github.com/gin-contrib/static v0.0.0-20190511124741-c1cdf9c9ec7b
+	github.com/gin-gonic/gin v1.4.0
+	github.com/joho/godotenv v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./webapp", false)))
+	router.Run()
+
+}
